8fa056816e569e9222b5eab67c582e62acda823a
Lily <lily.silva@goodplay.com>
2022-04-05 16:05:38 +0100

    Add background to divs on mouseenter

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 'use strict'
 
-const cdiv = document.querySelector('.canvas');
+const canvas = document.querySelector('.canvas')
 
 // Add 16 divs
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
-  div.style.cssText = "border: 1px solid #bbb; height: 25px; width: 25px";  
-  cdiv.appendChild(div);
+  div.style.cssText = "border: 1px solid #bbb; height: 25px; width: 25px;";  
+  div.setAttribute('class', 'hover')
+  canvas.appendChild(div);
+
 }
+
+let hover = document.querySelectorAll('.hover')
+let hoverList = [...hover]
+
+hoverList.map(el => el.addEventListener('mouseenter', () => {
+  el.classList.add('red')
+} ))
