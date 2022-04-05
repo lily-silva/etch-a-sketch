55f5b8c0453e4825e753cfc54ba33fec6f6abad0
Lily <lily.silva@goodplay.com>
2022-04-05 16:25:14 +0100

    Add button that erases canvas background

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 'use strict'
 
-const canvas = document.querySelector('.canvas')
-
 // Add 16 divs
+const canvas = document.querySelector('.canvas')
 for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div');
-  div.style.cssText = "border: 1px solid #bbb; height: 25px; width: 25px;";  
+  const div = document.createElement('div')
+  div.style.cssText = 'border: 1px solid #bbb; height: 25px; width: 25px;'
   div.setAttribute('class', 'hover')
-  canvas.appendChild(div);
-
+  canvas.appendChild(div)
 }
 
+// Add background to divs on mouseenter
 let hover = document.querySelectorAll('.hover')
 let hoverList = [...hover]
+hoverList.map((el) => {
+  el.addEventListener('mouseenter', () => {
+    el.classList.add('red')
+  })
+})
 
-hoverList.map(el => el.addEventListener('mouseenter', () => {
-  el.classList.add('red')
-} ))
+// Button that erases canvas background
+function erase() {
+  // console.log('click')
+  hoverList.map((el) =>
+    el.addEventListener('mouseenter', () => {
+      el.classList.remove('red')
+    })
+  )
+}
