b57a006fd731cfc1cc98825554775eb6affce988
Lily <lily.silva@goodplay.com>
2022-04-07 15:02:42 +0100

    Create random pastel colors Create new grid based on user input

--- script.js
+++ script.js
@@ -1,43 +1,85 @@
 'use strict'
 
 // Add 16 divs
-const canvas = document.querySelector('.canvas')
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div')
-  div.style.cssText = 'border: 1px solid #bbb; height: 25px; width: 25px;'
-  div.setAttribute('class', 'hover')
-  canvas.appendChild(div)
+
+function getColor(){ 
+  let R = Math.floor((Math.random() * 100) + 127);
+  let G = Math.floor((Math.random() * 127) + 127);
+  let B = Math.floor((Math.random() * 127) + 127);
+  
+  let rgb = (R << 16) + (G << 8) + B;
+  return `#${rgb.toString(16)}`;   
 }
 
-let hover = document.querySelectorAll('.hover')
-let hoverList = [...hover]
+function defaultGrid() {
+  const canvas = document.querySelector('.canvas')
+  const gridSize = getComputedStyle(document.documentElement)
+  document.getElementById('error').style.visibility = 'hidden'
+  let defCol = gridSize.getPropertyValue('--columns')
+  let defRow = gridSize.getPropertyValue('--rows')
+  for (let i = 0; i < defCol * defRow; i++) {
+    const div = document.createElement('div')
+    div.style.cssText = 'border: 1px solid #e3e3e3'
+    div.setAttribute('class', 'hover')
+    canvas.appendChild(div)
+  }
+  draw()
+}
 
-// Add background to divs on mouseenter
-function start() {
+function draw() {
+  let hover = document.querySelectorAll('.hover')
+  let hoverList = [...hover]
   hoverList.map((el) => {
     el.addEventListener('mouseenter', () => {
-      el.style.backgroundColor = '#555';
+      el.style.backgroundColor = '#dce0e0'
     })
   })
 }
+
+function bigGrid(e) {
+  const gridSize = document.getElementById('gridSize')
+  let size = gridSize.value
+  const errorText = document.getElementById('error')
+  errorText.style.visibility = 'hidden'
+  if (e.keyCode === 13) {
+    if (size >= 1 && size <= 64) {
+      const gridEls = document.querySelectorAll('.hover')
+      gridEls.forEach((el) => el.remove())
+      document.documentElement.style.setProperty('--columns', size)
+      document.documentElement.style.setProperty('--rows', size)
+      defaultGrid()
+    } else if (size !== '') {
+      errorText.style.visibility = 'visible'
+    }
+  }
+}
+
 // Revert background-color to transparent
-function revertBg () {
-  hoverList.map(el => el.style.backgroundColor = 'revert')
+function revertBg() {
+  let hover = document.querySelectorAll('.hover')
+  let hoverList = [...hover]
+  hoverList.map((el) => (el.style.backgroundColor = 'revert'))
 }
 
 // Generate random color on hover button
 function colors() {
+  let hover = document.querySelectorAll('.hover')
+  let hoverList = [...hover]
   hoverList.map((el) => {
     el.addEventListener('mouseenter', () => {
-      el.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+      el.style.backgroundColor =
+      getColor()
     })
   })
 }
 
+// Trigger b&w color button
 function bw() {
+  let hover = document.querySelectorAll('.hover')
+  let hoverList = [...hover]
   hoverList.map((el) => {
     el.addEventListener('mouseenter', () => {
-      el.style.backgroundColor = '#555';
+      el.style.backgroundColor = '#555'
     })
   })
 }
@@ -45,9 +87,11 @@ function bw() {
 // Button that cleans canvas background
 function reset() {
   revertBg()
+  let hover = document.querySelectorAll('.hover')
+  let hoverList = [...hover]
   hoverList.map((el) => {
     el.addEventListener('mouseenter', () => {
-      el.style.backgroundColor = '#555';
+      el.style.backgroundColor = '#555'
     })
   })
 }
